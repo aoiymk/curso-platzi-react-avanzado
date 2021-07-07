13d655bf3d06845730d8670465520e8f813426cd
La Elizabeth <aoi.yamiko07@gmail.com>
2021-07-06 22:27:15 -0400

    Adding new component: List of PhotoCards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
-import { PhotoCard } from './components/PhotoCard'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 export const App = () => (
   <div>
     <GlobalStyle />
     <ListOfCategories />
-    <PhotoCard />
+    <ListOfPhotoCards />
   </div>
 )
